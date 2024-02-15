c732cc1d245abe193c3f315303147c401d879e1c
tbehr <behr@rhrk.uni-kl.de>
2024-02-15 11:11:28 +0100

    Refactor Player

--- games/pong/main.py
+++ games/pong/main.py
@@ -1,7 +1,33 @@
 import pygame
 
-pygame.init()
 
+class GameObject:
+
+    def __init__(self, rect:pygame.Rect, speed:pygame.Vector2):
+        self.rect = rect
+        self.speed = speed        
+        self.x = rect.left
+        self.y = rect.top
+
+    # def get_rect(self):
+    #     return self.rect
+    
+    # def get_speed(self):
+    #     return self.speed
+
+    def set_position(self, x, y):
+        self.rect.left = x
+        self.rect.top = y
+
+    def set_speed(self, speed_x, speed_y):
+        self.speed.x = speed_x
+        self.speed.y = speed_y
+
+    def update(self,deltaTime):
+        self.set_position(self.x+ (self.speed.x * deltaTime), self.y + (self.speed.y * deltaTime))
+
+
+pygame.init()
 SCALE = 20
 
 screen = pygame.display.set_mode((40*SCALE, 40*SCALE))
@@ -9,19 +35,22 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+PLAYER_SPEED = 20*SCALE
+BALL_SPEED = 30*SCALE
 
-player_pos = pygame.Vector2(screen.get_width() / 10, screen.get_height() / 2)
-ball_pos = 0
-PLAYER_SPEED = 400
-BALL_SPEED = 600
+player = GameObject(pygame.Rect(0,screen.get_height()/2,1*SCALE,6*SCALE),pygame.Vector2(0,PLAYER_SPEED))
 
+#player_pos = pygame.Vector2(screen.get_width() / 10, screen.get_height() / 2) #comment due to player object
+
+
+ball_pos = pygame.Vector2((screen.get_width()/2), (screen.get_height()/2))
 playing_field = pygame.Rect(0,0,32*SCALE,32*SCALE)
 
+next_tick_ball = pygame.Vector2(0,0)
 
 def withinBoard(rect:pygame.Rect):
     return playing_field.contains(rect)
 
-size= 0
 
 while running:
     # poll for events
@@ -33,29 +62,38 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    player_box = pygame.Rect(0,player_pos.y,1*SCALE,6*SCALE+size)
-    #ball_box = pygame.Rect(ball_pos.x,ball_pos.y,ball_pos.xx,ball_pos.yy)
+    wall = pygame.Rect(32*SCALE, 0, 1*SCALE, 32*SCALE)
+    ball_box = pygame.Rect(ball_pos.x,ball_pos.y,10,10)
+    #player_box = pygame.Rect(0,player_pos.y,1*SCALE,6*SCALE+size) #comment due to player object
 
     #draw gameobjects
+    pygame.draw.rect(screen, "purple", wall)
     pygame.draw.rect(screen, "green", playing_field)
-    pygame.draw.rect(screen, "white", player_box)
-    #pygame.draw.rect(screen, "white", ball_box)
+    pygame.draw.rect(screen, "white", player.rect)
+    pygame.draw.rect(screen, "red", ball_box)
 
     #update variable for gameobjects
     #player movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] and withinBoard(player_box):
-        if (player_pos.y - PLAYER_SPEED * dt) < 0:
-            player_pos.y = 0
+    if keys[pygame.K_w] and withinBoard(player.rect):
+        if (player.rect.top - (player.speed.y * dt)) < 0:
+            player.rect.top = 0
         else:
-            player_pos.y -= PLAYER_SPEED * dt
-    if keys[pygame.K_s] and withinBoard(player_box):
-        if ((player_pos.y+player_box.height) + PLAYER_SPEED * dt) > (32*SCALE):
-            player_pos.y = 32*SCALE-player_box.height
-        else:
-            player_pos.y += PLAYER_SPEED * dt
+            player.rect.top -= player.speed.y * dt
 
+    if keys[pygame.K_s] and withinBoard(player.rect):
+        if (player.rect.bottom + (player.speed.y * dt)) > (32*SCALE):
+            player.rect.top=32*SCALE-player.rect.height
+        else:
+            player.rect.top += player.speed.y* dt
+
+
+    
+    #ball_box.
     #ball movement
+    
+    # next_tick_ball.x = ball_pos.x + BALL_SPEED * dt
+    # next_tick_ball.y = ball_pos.y + BALL_SPEED * dt
     
     #if ball collision  
     #if     player collide x flip
