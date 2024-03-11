4b513d68f9e413bfe9ae105efa2c22450357aaa8
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-03-11 14:36:51 +0100

    further joystick tests

--- tests/joystick.py
+++ tests/joystick.py
@@ -12,3 +12,8 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 print("Found:", joystick.get_name())
+for i in range(256):
+    print("axis: ",joystick.get_axis(i))
+    print("hat: ",joystick.get_hat(i))
+print(joystick.get_id())
+print(joystick.get_guid())
