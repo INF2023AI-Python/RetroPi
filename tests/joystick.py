f9cd10503f497795c6679cd46c04d7d7d4007186
marc <schoenigm.tin23@student.dhbw-heidenheim.de>
2024-03-11 14:42:08 +0100

    loop joystick test

--- tests/joystick.py
+++ tests/joystick.py
@@ -11,9 +11,12 @@ pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-print("Found:", joystick.get_name())
-for i in range(256):
-    print("axis: ",joystick.get_axis(i))
-    print("hat: ",joystick.get_hat(i))
-print(joystick.get_id())
-print(joystick.get_guid())
+running = True
+while running:
+    print("Found:", joystick.get_name())
+    for i in range(256):
+        print("axis: ",joystick.get_axis(i))
+        print("hat: ",joystick.get_hat(i))
+    print(joystick.get_id())
+    print(joystick.get_guid())
+
