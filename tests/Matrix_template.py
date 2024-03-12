4deee2e28683f80f62c705696c430c04242b2f4a
anse <anian.seidl@redlion.net>
2024-03-12 17:52:08 +0100

    changed order to make more sense

--- tests/Matrix_template.py
+++ tests/Matrix_template.py
@@ -12,9 +12,11 @@ options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
 
+matrix = RGBMatrix(options=options)
+
 image = Image.new("RGB", (32, 32))
 draw = ImageDraw.Draw(image)
-matrix = RGBMatrix(options=options)
+
 running = True
 while running:
     draw.rectangle((0,0,32,32),fill=(222,0,0,0))
