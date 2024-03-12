da29e9f65ed8956fb5cb0f4438b28626cd6d922c
Totob <96802845+toto-b@users.noreply.github.com>
2024-03-12 18:32:39 +0100

    game logik version 1

--- space_invaders/objects.py
+++ space_invaders/objects.py
@@ -1,4 +1,5 @@
 # Game Object Classes
+import random
 
 class Field:
     def __init__(self,x_size,y_size):
@@ -34,26 +35,40 @@ class Entity:
 
 class Mob(Entity):
 
-    def __init__(self, x_pos, y_pos, x_size, y_size, max_hp, size, attack_speed, value, speed):
+    def __init__(self, x_pos, y_pos, x_size, y_size, max_hp, attack_cooldown, value, speed, cooldown, color):
         super().__init__(x_pos, y_pos, x_size, y_size)
         
         self.max_hp = max_hp
         self.hp = max_hp
         
-        self.size = size
-        self.attack_speed = attack_speed
+        self.attack_cooldown = attack_cooldown
+        self.cooldown = cooldown
         self.value = value
         self.speed = speed
+
+        self.bullet = Bullet(-1,-1,0,0,0,0,0)
+        self.bullet.die()
+
+        self.color=color
 
     def take_dmg(self, dmg):
         self.hp = self.hp - dmg
         if self.hp <= 0:
             self.die()
     
-
     def move(self, time, x_dir, y_dir):
-        self.set_x_pos(x_dir*time*self.speed)
-        self.set_y_pos(y_dir*time*self.speed)
+        self.set_x_pos(self.x[0]+x_dir*time*self.speed)
+        self.set_y_pos(self.y[0]+y_dir*time*self.speed)
+
+    def shoot(self,time):
+        self.cooldown = self.cooldown + time
+        if self.cooldown > self.attack_cooldown:
+            if not self.bullet.is_alive():
+                self.cooldown = 0
+                if random.randint(1,9) <= 2:
+                    x_place = (self.x[0] + self.x[1])/2
+                    self.bullet = Bullet(x_place, self.y[1]+1, 0, 1, dmg = 1, speed = 2, direction = 1)
+            
 
 class Bullet(Entity):
 
@@ -78,7 +93,7 @@ class Player(Entity):
         self.hp = max_hp
         self.color = [0,255,0]
         self.shooting = False
-        self.bullet = Bullet(0,0,0,0,0,0,0)
+        self.bullet = Bullet(-1,-1,0,0,0,0,0)
         self.bullet.die()
 
     def take_dmg(self, dmg):
@@ -99,15 +114,15 @@ class Player(Entity):
         if self.x[1] > self.field.x_size:
             self.x[1] = self.field.x_size
             self.x[0] = self.x[1] - self.x_size
-        elif self[0] < 0:
+        elif self.x[0] < 0:
             self.x[0] = 0
             self.x[1] = self.x_size
 
     def shoot(self):
         if not self.bullet.is_alive():
-            # TODO Bullet spawn place
-            self.bullet = Bullet(self.x[0], self.y[0], self.x_size, self.y_size, dmg = 1, speed = 2, direction = -1)
-            #return self.bullet 
+            x_place = (self.x[0] + self.x[1])/2
+            self.bullet = Bullet(x_place, self.y[0]-2, 0, 1, dmg = 1, speed = 2, direction = -1)
+            
 
 class Rock(Entity):
 
@@ -115,11 +130,11 @@ class Rock(Entity):
         super().__init__(x_pos, y_pos, x_size, y_size)
         self.max_hp = max_hp
         self.hp = max_hp
-        self.color=[0,255,0]
+        self.color=[105,105,105]
 
-    # TODO: add color
     def take_dmg(self, dmg):
         self.hp = self.hp - dmg
+        self.color = [105*(self.hp/self.max_hp)]*3
         if self.hp <= 0:
             self.die()
 
@@ -139,3 +154,67 @@ class Base(Entity):
         
         if self.hp <= 0:
             self.die()
+
+
+class MobList:
+    # list_of_list_of_mobs [[col1_mobs],[col2_mobs],...]
+    def __init__(self):
+        self.list = [[],[],[],[]]
+        self.dead_columns = [False, False, False, False]
+        self.wave = 0
+
+    def add_row(self, row_of_mobs):
+        if len(row_of_mobs) != len(self.list):
+            print("Amount of Mobs in Row doesn't match the amount of columns.")
+            return
+
+        for columns, row_elements zip(self.list, row_of_mobs):
+            columns.append(row_elemnts)
+        
+    def update(self):
+        len_list = len(self.list)-1
+        if len_list < 0:
+            print("List of Mobs is to short.")
+            return
+
+        # for each mob: if a mob is behind the mob, it is deleted and the mob behind is pushed
+        # ahead,allowing it to shoot.
+        for i in range(len_list):
+            if not self.list[i][0].is_alive():
+                if len(self.list[i]) > 0:
+                    del self.list[i][0]
+                
+                # to save, if all mobs of a columns are dead
+                else:
+                    self.dead_columns[i] = True
+
+    def get_first_row(self):
+        result = []
+        for columns in self.list:
+            result.append(columns[0])
+        return result
+    
+    def get_all(self):
+        singular_list = [item for sublist in self.list for item in sublist]
+        return singular_list
+    
+    def all_dead():
+        a = True
+        for col_status in self.dead_columns:
+            a = a and col_status
+        return a
+    
+    def reset_dead_columns(self):
+        self.dead_columns = [False, False, False, False]
+        
+# useless da nicht jeder move 1 pixel ist
+# class MobMove:
+
+#     def __init__(self,steps):
+#         self.step = -1
+#         self.len_steps = len(steps)
+#         self.steps = steps
+    
+#     def take_step(self):
+#         self.step = ((self.step+1) % self.len_steps)
+#         return self.steps[self.step]
--- space_invaders/space_invaders.py
+++ space_invaders/space_invaders.py
@@ -1,80 +1,225 @@
-import objects    
+import pygame
+import objects
 # Improvements für objects:
 # nur ein globales Field
 # bullet, mob, player, base, rock colors machen
 
-"""
-Idee für gameloop und drawen von einzelnen objekten:
-    for object in list_objects:
-        if object.is_alive():
-            draw(object)
-"""
-
 # TODO implement
-def game_loss(score):
+def lose(score):
     print(score)
 
-# TODO: implement next_wave
-def next_wave(mob_list):
-    pass
 
-# TODO - aktuelles Problem: Bullets checken nur das komplette Rectangle
+# aktuelles Problem: Bullets checken nur das komplette Rectangle
 def collision(entity, bullet):
     if entity.x[0] <= bullet.x[0] and bullet.x[0] <= entity.x[1]:
         if bullet.y[1] >= entity.y[0]:
             entity.take_dmg(bullet.dmg)
             bullet.die()
 
+def collision_rock_mob(rock, mob):
+    if rock.x[0] <= mob.x[0] and mob.x[0] <= rock.x[1]:
+        if mob.y[1] >= rock.y[0]:
+            mob.die()
+            rock.take_dmg(mob.max_hp)
+
 # TODO - Implement draw function
 # move funktion aller entities gleiche signatur machen
 def draw(entity):
     pass
+
+# TODO delte when useles:
+def temp():
+    """
+    00000000000000000000000000000000
+    00011111001111100111110011111000
+    00011111001111100111110011111000
+    00011111001111100111110011111000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00011111001111100111110011111000
+    00011111001111100111110011111000
+    00011111001111100111110011111000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00011111001111100111110011111000
+    00011111001111100111110011111000
+    00011111001111100111110011111000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00000000000000000000000000000000
+    00001111000001110000000001111100
+    00001111000001110000000001111100
+    00000000000000000000000000000000
+    00000000000PPPPP0000000000000000
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    """
+    pass
+
+def next_wave(moblist: MobList):
+    wave = moblist.wave
+    mobA_list = []
+    mobB_list = []
+    mobC_list = []
+    value_mobA =  int(10*(1.2**wave))
+    value_mobB =  int(10*(1.2**(wave+1)))
+    value_mobC =  int(10*(1.2**(wave+2)))
+    attack_cooldown_mobA = max(3,5-0.1*wave)
+    attack_cooldown_mobB = max(1,3-0.1*wave)
+    attack_cooldown_mobC = max(2,4-0.1*wave)
+
+    for i in range(4):
+        x_cords = 3+i*7
+        mobA = Mob(x_cords,1,4,2, 1, attack_cooldown_mobA, value_mobA, 2, 3, [161, 8, 8])
+        mobA_list.append(mobA)
+
+        mobB = Mob(x_cords,6,4,2, 1, attack_cooldown_mobB, value_mobB, 2, 2, [92, 29, 140])
+        mobB_list.append(mobB)
+        
+        mobC = Mob(x_cords,11,4,2, 2, attack_cooldown_mobC, value_mobC, 2, 4, [40, 29, 140])
+        mobC_list.append(mobC)
+
+    moblist.add_row(mobA_list)
+    moblist.add_row(mobB_list)
+    moblist.add_row(mobC_list)
+    moblist.reset_dead_columns()
+
+    # if wave = 1:
+    # # 1 Wave:
+    #     mob1A = Mob(x,y,x+,y+, 1, 5, 10, 2, 3, [161, 8, 8])
+    #     mob1B = Mob(x,y,x+,y+, 1, 3, 12, 2, 2, [92, 29, 140])
+    #     mob1C = Mob(x,y,x+,y+, 1, 4, 14, 2, 4, [40, 29, 140])
+    # if wave = 2:
+    #     # 2 Wave:
+    #     mob2A = Mob(x,y,x+,y+, 1, 4.9, 12, 2, 3, [161, 8, 8])
+    #     mob2B = Mob(x,y,x+,y+, 1, 2.9, 14, 2, 2, [92, 29, 140])
+    #     mob2C = Mob(x,y,x+,y+, 1, 3.9, 16, 2, 4, [40, 29, 140])
+    # if wave = 3:
+    # # 3 Wave:
+    #     mob3A = Mob(x,y,x+,y+, 1, 4.8, 14, 2, 3, [161, 8, 8])
+    #     mob3B = Mob(x,y,x+,y+, 1, 2.8, 16, 2, 2, [92, 29, 140])
+    #     mob3C = Mob(x,y,x+,y+, 1, 3.8, 19, 2, 4, [40, 29, 140])
+
+def reset_rocks(rock_list):
+    
+    rock_list[0] = Rock(4,24,3,2,8)
+    rock_list[1] = Rock(13,24,2,2,6)
+    rock_list[2] = Rock(25,24,4,2,10)
+
 
 # Game
 player = objects.Player(14,30,5,0,max_hp=3)
 base = objects.Base(10)
 score=0
 running=True
-mob_list=[]
+mob_list = objects.MobList([])
 bullet_list=[]
+bullet_list.append(player.bullet)
+
 rock_list=[]
+pygame.init()
+SCALE=12
+# screen = pygame.display.set_mode((32*SCALE, 32*SCALE))
+clock = pygame.time.Clock()
+dt = 0
+
+
+
+
 
 while running:
 
-    # Player Moves
-    # Mobs Move
+    # TODO
+    # # Player Moves
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_a]:
+    #     print("a")
+    #     player.move(dt,1,-1)
+    #     print(player.x)
+
+    # if keys[pygame.K_d]:
+    #     print("d")
+    #     player.move(dt,1,1)
+    #     print(player.x)
+    
+    # Mobs Move (only move forward atm)
+    for mob in mob_list.get_all():
+        mob.move(dt,0,-1)
+    
+    
     # Bullets Move
+    for bullet in bullet_list:
+        bullet.travel(dt)
+
+    # Bullets are shot
+    # Player Bullet is shot
+    if keys[pygame.K_w]:
+        player.shoot()
+
+    bullet_list = []
+    if player.bullet.is_alive():
+        bullet_list.append(player.bullet)
+    
+    # Mob Bullets are shot
+    for mob in mob_list.get_first_row():
+        mob.shoot(dt)
+        bullet_list.append(mob.bullet)
 
     # Bullet-Collisions
     for bullet in bullet_list:
+        
         # Bullet Player Collision
-        if bullet.is_alive():
-            collision(player, bullet)
+        # If bullet isnt alive: skip the iteration
+        if not bullet.is_alive():
+            continue
+        collision(player, bullet)
 
         # Bullet Base Collision
-        if bullet.is_alive():
-            collision(base, bullet)
-
+        if not bullet.is_alive():
+            continue
+        collision(base, bullet)
+            
         # Bullet Rock Collisions
+        if not bullet.is_alive():
+            continue
         for rock in rock_list:
-            if bullet.is_alive():
+            if bullet.is_alive() and rock.is_alive():
                 collision(rock,bullet)
 
         # Bullet Mob Collisions
-        for mob in mob_list:
-            if bullet.is_alive():
+        if not bullet.is_alive():
+            continue
+        for mob in mob_list.get_firt_row():
+            if bullet.is_alive() and mob.is_alive():
                 collision(mob, bullet)
-        
+                if not mob.is_alive():
+                    score = score + mob.value
+    
+    # Mob Rock Collision
+    for mob in mob_list.get_firt_row:
+        if mob.is_alive():
+            for rock in rock_list:
+                if rock.is_alive():
+                    collision_rock_mob(rock,mob)
+
+    # adjust current mobs
+    mob_list.update()
+
     # Check Lose Condition
     if (not player.is_alive()) or (not base.is_alive()):
-        print("B")
-    # Debug:
-    a = input("Waiting")
+        lose(score)
+
 
     # If all Mob objects are dead => new wave
-    all_mob_status = [x.is_alive() for x in mob_list]
-
-    if True not in all_mob_status:
+    if mob_list.all_dead():
+        reset_rocks(rock_list)
         next_wave(mob_list)
     
     # Draw all Objects:
@@ -82,4 +227,13 @@ while running:
         draw(entity)
 
 
-a = input("Exit?")
+    dt = clock.tick(60) / 1000
+    print("dt: ", dt)
+
+
+# b = objects.Bullet(15,5,0,0,5,2,1)
+# print("Position: ",b.x,b.y)
+# print("IsAlive?: ",b.is_alive())
+# b.travel(20)
+# print("Position: ",b.x,b.y)
+# print("IsAlive?: ",b.is_alive())
