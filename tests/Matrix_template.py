f174dde20d39c8475c3b12f5377ad78826e8c862
anse <anian.seidl@redlion.net>
2024-03-12 16:53:28 +0100

    added Matrix implementation Template

---
+++ tests/Matrix_template.py
@@ -0,0 +1,21 @@
+import pygame
+from PIL import Image
+from PIL import ImageDraw
+try:
+    from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+except ImportError:
+    from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+options = RGBMatrixOptions()
+options.rows = 32
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+matrix = RGBMatrix(options=options)
+running = True
+while running:
+    image = Image.new("RGB", (32, 32))
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((0,0,32,32),fill=(222,0,0,0))
+    matrix.SetImage(image, 0, 0)
