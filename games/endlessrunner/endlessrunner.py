27339f195d8a3f5b6e08c5e6cb7203b76bbfae6b
Totob <96802845+toto-b@users.noreply.github.com>
2024-03-20 12:06:23 +0100

    Reworked endlessrunner; Refactored to runner

--- games/endlessrunner/endlessrunner.py
+++ games/endlessrunner/endlessrunner.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 from PIL import Image
 from PIL import ImageDraw
 try:
@@ -23,12 +24,16 @@ SCALE = 1
 TOP_FLOOR = 13
 BOTTOM_FLOOR = 29
 
-player_x = 3*SCALE
-player_y = (BOTTOM_FLOOR-1)*SCALE
+player_x = 3
+player_y = (BOTTOM_FLOOR-1)
 y_change = 0
 gravity = 1
 score = 0
 speedaddition = 0
+SPEED = 0.2
+obstacle_speed = SPEED
+# obstacle_speed velo
+dt = 0
 running = True
 clock = pygame.time.Clock()
 
@@ -43,21 +48,27 @@ except Exception:
     joystick_found = False
     print("Kein Joystick gefunden")
 
+def temp_get_dist(obstacle,player_x,player_y):
+    x_distance = obstacle[0]-player_x
+    y_distance = obstacle[1]-player_y
+    distance = math.sqrt(x_distance*x_distance + y_distance*y_distance)
+    return distance
+
 def draw_floor():
     draw.rectangle((0,BOTTOM_FLOOR,32,BOTTOM_FLOOR),fill=(255,255,255))
     draw.rectangle((0,TOP_FLOOR,32,TOP_FLOOR),fill=(255,255,255))
 
 def spawn_random_obstacle():
-    obstacle_x = (WIDTH-1)*SCALE
-    obstacle_y = random.randint(TOP_FLOOR+1, BOTTOM_FLOOR-1)*SCALE
+    obstacle_x = (WIDTH-1)
+    obstacle_y = random.randint(TOP_FLOOR+1, BOTTOM_FLOOR-1)
     obstacles.append((obstacle_x, obstacle_y))
 
 def spawn_fixed_obstacle():
-    obstacle_x = (WIDTH-1)*SCALE
-    obstacle_y = BOTTOM_FLOOR-1*SCALE
+    obstacle_x = (WIDTH-1)
+    obstacle_y = BOTTOM_FLOOR-1
     obstacles.append((obstacle_x, obstacle_y))
-    obstacle_x = (WIDTH-1)*SCALE
-    obstacle_y = TOP_FLOOR+1*SCALE
+    obstacle_x = (WIDTH-1)
+    obstacle_y = TOP_FLOOR+1
     obstacles.append((obstacle_x, obstacle_y))
 
 def draw_obstacles():
@@ -66,12 +77,15 @@ def draw_obstacles():
 
 def collision(): #just checks for a collision from the front
     for obstacle in obstacles:
-        if player_x <= obstacle[0] <= player_x + SCALE and player_y <= obstacle[1] <= player_y + SCALE:
+        # if player_x <= obstacle[0] <= player_x + SCALE and player_y <= obstacle[1] <= player_y + SCALE:
+        # if player_x <= obstacle[0] <= player_x and player_y <= obstacle[1] <= player_y :
+        if int(player_x) == int(obstacle[0]) and int(player_y) == int(obstacle[1]):
             return True
     return False
 
 image = Image.new("RGB", (32, 32))
 draw = ImageDraw.Draw(image)
+spawn_timer = 0
 
 while running:
     for event in pygame.event.get():
@@ -95,16 +109,25 @@ while running:
                     gravity = 1
                     y_change = -1
 
-    if player_y < (BOTTOM_FLOOR-1)*SCALE or y_change > 0 or player_y > (TOP_FLOOR+1)*SCALE:
-        player_y -= y_change
+    if player_y < (BOTTOM_FLOOR-1) or y_change > 0 or player_y > (TOP_FLOOR+1):
+        player_y -= y_change*SPEED
         y_change -= gravity
-    if gravity > 0 and player_y > (BOTTOM_FLOOR-1)*SCALE:
-        player_y = (BOTTOM_FLOOR-1)*SCALE
+    if gravity > 0 and player_y > (BOTTOM_FLOOR-1):
+        player_y = (BOTTOM_FLOOR-1)
         y_change = 0
-    if gravity < 0 and player_y < (TOP_FLOOR+1)*SCALE:
-        player_y = (TOP_FLOOR+1)*SCALE
+    if gravity < 0 and player_y < (TOP_FLOOR+1):
+        player_y = (TOP_FLOOR+1)
         y_change = 0
 
+    obstacles = [(x-obstacle_speed, y) for x, y in obstacles if x > 0]
+    for obstacle in obstacles:
+        distance = temp_get_dist(obstacle,player_x,player_y)
+        if distance <= 2:
+            print(distance)
+        if distance <= 1:
+            running = False
+            print("Lost")
+            # print(distance)
 
     draw.rectangle((0,0,32,32),fill=(0,0,0,0))
     draw.rectangle((player_x, player_y, player_x, player_y), fill=(255, 255, 255))
@@ -112,24 +135,29 @@ while running:
     draw_obstacles()
     matrix.SetImage(image, 0, 0)
 
-    if random.randint(0, 100) < 10:
-        if random.randint(0, 100) < 75:
-            spawn_random_obstacle()
-        else:
-            pass
+    if spawn_timer <= 0:
+        spawn_timer = 0.85
+        # if random.randint(0, 100) < 33:
+        spawn_random_obstacle()
+    else:
+        spawn_timer = spawn_timer - dt
+    
 
-    obstacles = [(x-1, y) for x, y in obstacles if x > 0]
+    # if collision():
+    #     print("Game Over")
+    #     running = False
 
-    if collision():
-        print("Game Over")
-        running = False
+    score += dt
+    # print(int(score)) 
 
-    score += 1
-    print(score)
-
-    if random.randint(0, 100) < 1:
-        speedaddition += 1
+    # if random.randint(0, 100) < 1:
+    #     print("Faster!")
+    #     obstacle_speed *= 1.1
     #pygame.time.delay(1000//10+speedaddition)
-    clock.tick(30)
+    # clock.tick(60)
+    obstacle_speed = math.log(max(5,int(score)),30)
+
+
+    dt = clock.tick(60)/1000
 
 pygame.quit()
