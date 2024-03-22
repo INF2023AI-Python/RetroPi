5d6b0607debf0dd544bb45e645702e09f3ffad2e
itsAnian <155064927+itsAnian@users.noreply.github.com>
2024-03-22 14:31:57 +0100

    added autoexit & removed debug prints

--- button_test.py
+++ button_test.py
@@ -1,9 +1,10 @@
 import pygame
+import time
 
 def start_button_test(matrix, joystick_found, joystick, draw, image):
     pygame.init()
     running = True
-
+    
     YELLOW = (255, 255, 0)
     BLUE = (0, 0, 255)
     GREY = (128, 128, 128)
@@ -54,33 +55,43 @@ def start_button_test(matrix, joystick_found, joystick, draw, image):
         draw.line((22, 19, 24, 21), fill=color)
         draw.line((24, 21, 26, 19), fill=color)
 
-    def joystick_inputs(x_axis, y_axis, threshold=0.1):
+    def joystick_inputs(x_axis, y_axis, exit_timer, threshold=0.1):
         x_axis = 0 if abs(x_axis) < threshold else x_axis
         y_axis = 0 if abs(y_axis) < threshold else y_axis
         if x_axis < 0:
             draw_joystick_right(WHITE)
+            exit_timer = 5
         if x_axis > 0:
             draw_joystick_left(WHITE)
+            exit_timer = 5
         if y_axis < 0:
             draw_joystick_down(WHITE)
+            exit_timer = 5
         if y_axis > 0:
             draw_joystick_up(WHITE)
+            exit_timer = 5
 
         if joystick.get_button(8):
-            print("8")
             draw.rectangle((10, 14, 12, 16), fill=YELLOW)
+            exit_timer = 5
         if joystick.get_button(9):
             draw.rectangle((6, 9, 8, 11), fill=BLUE)
-            print("9")
+            exit_timer = 5
         if joystick.get_button(10):
-            print("10")
             draw.rectangle((2, 14, 4, 16), fill=YELLOW)
+            exit_timer = 5
         if joystick.get_button(11):
-            print("11")
             draw.rectangle((6, 19, 8, 21), fill=BLUE)
+            exit_timer = 5
+        return exit_timer
 
+    exit_timer = 5
+    dt = 0
+    clock = pygame.time.Clock()
     while running:
-
+        if exit_timer < 0:
+            running = False
+        pygame.event.get()
         draw.rectangle((0, 0, 31, 31), fill=BLACK)
         draw_button_8()
         draw_button_9()
@@ -94,6 +105,7 @@ def start_button_test(matrix, joystick_found, joystick, draw, image):
         if joystick_found:
             x_axis = joystick.get_axis(0)
             y_axis = joystick.get_axis(1)
-            joystick_inputs(x_axis, y_axis)
-
+            exit_timer = joystick_inputs(x_axis, y_axis, exit_timer)
+        exit_timer = exit_timer - dt
         matrix.SetImage(image, 0, 0)
+        dt = clock.tick(60)
