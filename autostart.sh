8ce6b11cde553df4724017b374441b11cedd8e23
itsAnian <155064927+itsAnian@users.noreply.github.com>
2024-03-25 15:13:34 +0100

    added shutdown

--- autostart.sh
+++ autostart.sh
@@ -6,6 +6,7 @@ while true; do
     if [ -f "$file_path" ]; then
         echo "File found trys to open "
         sudo python3 "$file_path"
+        sudo shutdown now
     else
         echo "No file found"
     fi
